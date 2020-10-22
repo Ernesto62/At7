719a692d2e86f4638ae96ee72d783e4420779a77
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-10-22 11:52:14 +0100

    Update cinco.php, diasemana.php, and 4 more files...

---
+++ numero.php
@@ -0,0 +1,21 @@
+<?php 
+$numero=50;
+
+switch(true){
+	case ($numero > 100):
+	echo 'valor muito alto';
+	break;
+	case ($numero > 80 && $numero > 51):
+	echo 'valor médio';
+	break;
+	case ($numero === 50):
+	echo 'valor perfeito';
+	break;
+	case ($numero <= 10 ):
+	echo 'valor muito baixo';
+	break;
+	case ($numero == 0):
+	echo 'sem valor';
+	break;
+}
+?>
